f7964b4515ba68c9ee4f99874c4623074fe7feaa
hunterskg <hunterskg1@gmail.com>
2025-10-16 21:53:37 +0700

    before pull

--- src/main/java/crm/admin/ViewRoleListServlet.java
+++ src/main/java/crm/admin/ViewRoleListServlet.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet(urlPatterns = "/ViewRoleList")
+@WebServlet(urlPatterns = "ViewRoleList")
 public class ViewRoleListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -136,6 +136,5 @@ public class ViewRoleListServlet extends HttpServlet {
             response.sendRedirect("ViewRoleList");
             return;
         }
-        doGet(request, response);
     }
 }
