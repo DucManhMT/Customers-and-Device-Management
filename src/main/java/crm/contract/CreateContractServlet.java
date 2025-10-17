354c184bed3fc858cb33d9c7cef9a045af3c3fac
hunterskg <hunterskg1@gmail.com>
2025-10-17 12:41:12 +0700

    Add contract creation logic and asset images

--- src/main/java/crm/contract/CreateContractServlet.java
+++ src/main/java/crm/contract/CreateContractServlet.java
@@ -1,6 +1,11 @@
 package crm.contract;
 
 import crm.common.URLConstants;
+import crm.common.model.Contract;
+import crm.common.model.Customer;
+import crm.core.config.DBcontext;
+import crm.core.repository.hibernate.entitymanager.EntityManager;
+import crm.core.service.IDGeneratorService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -8,6 +13,8 @@ import jakarta.servlet.annotation.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.Date;
+import java.time.LocalDate;
 
 @WebServlet(name = "CreateContract", value = URLConstants.CUSTOMER_SUPPORTER_CREATE_CONTRACT)
 @MultipartConfig(
@@ -25,6 +32,7 @@ public class CreateContractServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = new EntityManager(DBcontext.getConnection());
         Part filePart = request.getPart("contractImage");
         String fileName = Path.of(filePart.getSubmittedFileName()).getFileName().toString();
 
@@ -37,11 +45,27 @@ public class CreateContractServlet extends HttpServlet {
         String filePath = uploadPath + File.separator + fileName;
         filePart.write(filePath);
 
+        Contract contract = new Contract();
+        Customer customer = em.find(Customer.class,1);
+        contract.setContractID(IDGeneratorService.generateID(Contract.class));
+        contract.setContractImage(fileName);
+        contract.setStartDate(Date.valueOf(LocalDate.now()));
+        contract.setExpiredDate(Date.valueOf(LocalDate.now().plusYears(1)));
+
+        contract.setCustomerID(customer.getCustomerID());
+        contract.setCustomer(customer);
+        System.out.println("contract.getContractID(): " + contract.getContractID());
+        System.out.println("contract.getContractImage(): " + contract.getContractImage());
+        System.out.println("contract.getStartDate(): " + contract.getStartDate());
+        System.out.println("contract.getExpiredDate(): " + contract.getExpiredDate());
+        System.out.println(contract.getCustomer().getCustomerID());
+        em.persist(contract,Contract.class);
+
         // Gửi phản hồi hiển thị ảnh đã upload
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println("<html><body style='font-family:Arial;text-align:center;'>");
         response.getWriter().println("<h2>Upload Successful!</h2>");
-        response.getWriter().println("<img src='uploads/" + fileName + "' style='max-width:400px;border-radius:8px;'><br><br>");
+        response.getWriter().println("<img src='assets/" + fileName + "' style='max-width:400px;border-radius:8px;'><br><br>");
         response.getWriter().println("<a href='create_contract.jsp'>← Back to Upload</a>");
         response.getWriter().println("</body></html>");
     }
